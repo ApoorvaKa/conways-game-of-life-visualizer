a879200117dca4fd54d25e1c0cd7d4cf57d9df4e
Apoorva Kaushik <42742772+ApoorvaKa@users.noreply.github.com>
2020-10-21 22:14:12 -0500

    Updates

---
+++ precache-manifest.b207c489d6da994eeb7fb98cd2ddc9a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10f00d5969fd49a6c7d0c1f68a44e16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23d3104cc7f016394f93",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "be9129e1d25cff593fd6",
+    "url": "/static/js/2.ab14123b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ab14123b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d3104cc7f016394f93",
+    "url": "/static/js/main.1e94a987.chunk.js"
+  },
+  {
+    "revision": "c8fbbddf6059836bb8e7",
+    "url": "/static/js/runtime-main.eb688cb7.js"
+  }
+]);
