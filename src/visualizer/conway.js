1e923a90c2c65f4452d6ac156c73ae2fa3f2abc7
Apoorva Kaushik <42742772+ApoorvaKa@users.noreply.github.com>
2020-10-21 21:51:40 -0500

    Add files via upload

---
+++ src/visualizer/conway.js
@@ -0,0 +1,145 @@
+import React, { useCallback, useRef, useState } from 'react';
+import produce from "immer";
+const numRows = 30;
+const numCols = 30;
+
+var generation = 0;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+];
+
+
+const generateEmptyGrid = () => {
+  const rows = []
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0))
+  }
+  generation = 0;
+  return rows;
+}
+
+const generateRandomGrid = () => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(
+      Array.from(Array(numCols), () => (Math.random() > 0.6 ? 1 : 0))
+    );
+  }
+  generation = 0;
+  return rows;
+}
+
+const ConwaysVisualizer = () => {
+  const [grid, setGrid] = useState(() => {
+    return generateEmptyGrid();
+  });
+
+  const [running, setRunning] = useState(false);
+  const runningRef = useRef(running);
+  runningRef.current = running
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      generation=0;
+      return;
+    }
+
+    setGrid(g => {
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let j = 0; j < numCols; j++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newJ = j + y;
+              if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+                neighbors += g[newI][newJ];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][j] = 0;
+            } else if (g[i][j] === 0 && neighbors === 3) {
+              gridCopy[i][j] = 1;
+            }
+          }
+        }
+      });
+    });
+    generation += 1;
+    console.log("gen", generation);
+    setTimeout(runSimulation, 100);
+  }, []);
+
+
+  return (
+    <>
+
+      <button
+        onClick={() => {
+          setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSimulation()
+          }
+        }}>
+        {running ? 'Stop' : "Start"}
+      </button>
+
+      <button
+        onClick={() => {
+          setGrid(generateEmptyGrid())
+        }}>
+        Clear Grid
+      </button>
+
+      <button
+        onClick={() => {
+          setGrid(generateRandomGrid())
+        }}>
+        Randomize Grid
+      </button>
+      
+      <div>
+        Generation:  {generation}
+      </div>
+
+      <div
+        style={{
+          justifyContent: "center",
+          display: "grid",
+          gridTemplateColumns: `repeat(${numCols}, 20px)`
+        }}
+      >
+        {grid.map((rows, i) => rows.map((col, j) => (
+          <div
+            key={'${i}-${j}'}
+            onMouseDown={() => {
+              const newGrid = produce(grid, gridCopy => {
+                gridCopy[i][j] = grid[i][j] ? 0 : 1;
+              });
+              setGrid(newGrid);
+            }}
+            style={{
+              width: 20,
+              height: 20,
+              backgroundColor: grid[i][j] ? "dodgerBlue" : undefined,
+              border: "solid 1px black",
+            }}
+          />
+        ))
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ConwaysVisualizer;
